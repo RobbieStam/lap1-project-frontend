f6cce5e4207e31b8819c655cae9add4f429041f4
SamM71 <sammocock@gmail.com>
2023-07-31 15:38:53 +0100

    display random countries

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+const url = "https://capitals-quiz.onrender.com/countries";
+
+function getRandomCountry(countries) {
+  const max = countries.length;
+  const randIdx = Math.floor(Math.random() * max);
+  return countries[randIdx];
+}
+
+function fetchCountry(data) {
+
+  const country = getRandomCountry(data);
+
+  const textElement = document.querySelector("#question");
+  textElement.textContent = country['name'];
+}
+
+function displayCountry() {
+  fetch(url)
+  .then(resp => resp.json())
+  .then(fetchCountry);
+}
+
+displayCountry();
+
 const score = document.querySelector("#score")
 
 const randomScore = () => Math.floor(Math.random() *100);

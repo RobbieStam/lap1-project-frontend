a429d51af753df8e5a998c47a3565234894781c7
SamM71 <sammocock@gmail.com>
2023-08-01 10:31:16 +0100

    create static folder for js and css

---
+++ static/js/index.js
@@ -0,0 +1,51 @@
+const url = "https://capitals-quiz.onrender.com/countries";
+
+let currentCapital;
+let score = 0;
+const scoreText = document.querySelector("#score");
+
+function displayScore() {
+  scoreText.textContent = `Score: ${score}`
+}
+
+function getRandomCountry(countries) {
+  const max = countries.length;
+  const randIdx = Math.floor(Math.random() * max);
+  return countries[randIdx];
+}
+
+function fetchCountry(data) {
+
+  const country = getRandomCountry(data);
+
+  const textElement = document.querySelector("#question");
+  textElement.textContent = country['name'];
+
+  currentCapital = country['capital'];
+  console.log(currentCapital);
+}
+
+function displayCountry() {
+  fetch(url)
+  .then(resp => resp.json())
+  .then(fetchCountry);
+}
+
+function checkAnswer(e) {
+  e.preventDefault();
+  const input = e.target.answer.value;
+  if (input === currentCapital) {
+    score++;
+    console.log(score)
+    console.log("correct")
+  }
+  e.target.answer.value = '';
+  displayScore();
+  displayCountry();
+}
+
+const form = document.querySelector('#country-guess');
+form.addEventListener('submit', checkAnswer);
+
+displayScore();
+displayCountry();
